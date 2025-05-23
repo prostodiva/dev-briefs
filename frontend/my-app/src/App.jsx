0a8179df03f1707fd9551cb77240031dc50a85bc
prostodiva <rkattsyna@mail.ru>
2025-05-22 18:41:12 -0700

    setup the simple React app

---
+++ frontend/my-app/src/App.jsx
@@ -0,0 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage';
+
+const AppRoutes = () => {
+    return (
+        <Routes>
+            <Route path="/" element={<Navigate to="home" />} />
+            <Route path="/home" element={<HomePage />} />
+        </Routes>
+    );
+};
+
+const App = () => {
+  return (
+    <>
+        <AppRoutes />
+    </>
+  );
+};
+
+export default App;
---
+++ frontend/my-app/src/pages/HomePage.jsx
@@ -0,0 +1,17 @@
+
+
+const HomePage = () => {
+    return (
+        <div>
+            <main>
+                <section>
+                    <h1>Hi from the home page</h1>
+                </section>
+            </main>
+        </div>
+
+    );
+};
+
+export default HomePage;
+

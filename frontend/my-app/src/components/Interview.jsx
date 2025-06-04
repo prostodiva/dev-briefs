8a8a2367a27424ef51789597ab57e8b534200941
prostodiva <rkattsyna@mail.ru>
2025-06-04 09:34:19 -0700

    Prepare Interview component. Add flashcards section

---
+++ frontend/my-app/src/components/Interview.jsx
@@ -0,0 +1,11 @@
+
+const Interview = () => {
+    return (
+        <section>
+            <h1>MAKE A FLASHCARDS WITH DIFF TOPICS</h1>
+            <h1>Practice your answers recording yourself</h1>
+        </section>
+    );
+};
+
+export default Interview;
--- frontend/my-app/src/pages/InterviewPrepPage.jsx
+++ frontend/my-app/src/pages/InterviewPrepPage.jsx
@@ -1,4 +1,5 @@
 import Navbar from "../components/Navbar.jsx";
+import Interview from "../components/Interview.jsx";
 
 const InterviewPrepPage = () => {
     return (
@@ -6,6 +7,7 @@ const InterviewPrepPage = () => {
             <div>
                 <Navbar />
             </div>
+            <Interview />
         </section>
     );
 };

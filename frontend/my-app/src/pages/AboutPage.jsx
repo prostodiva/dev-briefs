20b3c8b8a3d39c648c0d84fe7ac3019dd38bfdc0
prostodiva <rkattsyna@mail.ru>
2025-06-11 22:49:19 -0700

    Fix the video

--- frontend/my-app/src/pages/AboutPage.jsx
+++ frontend/my-app/src/pages/AboutPage.jsx
@@ -19,7 +19,7 @@ const AboutPage = () => {
                             </div>
                         )}
                         <iframe
-                            src="https://youtu.be/KYUJ7cqIzXA"
+                            src="https://www.youtube.com/embed/KYUJ7cqIzXA"
                             title="YouTube video player"
                             frameBorder="0"
                             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"

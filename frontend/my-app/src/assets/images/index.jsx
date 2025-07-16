945d7154a834499f36de71725a32b4220689139d
prostodiva <rkattsyna@mail.ru>
2025-07-15 19:05:48 -0700

    Challenge 27

--- frontend/my-app/src/assets/images/index.jsx
+++ frontend/my-app/src/assets/images/index.jsx
@@ -22,8 +22,9 @@ import day23 from "./day23.png";
 import day24 from "./day23.1.png";
 import day25 from "./day25.png";
 import day26 from "./day26.png";
+import day27 from "./day27.png";
 import back4 from "./back4.png";
 
-export { day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, back4, day12, day13, day14, day15, day16, day19, day20, day21, day22, day23, day24, day25, day26 };
+export { day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, back4, day12, day13, day14, day15, day16, day19, day20, day21, day22, day23, day24, day25, day26, day27 };
 
 
